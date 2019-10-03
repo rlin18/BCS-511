c9729da6a454329a6d0a369b0ccdd87e808e93a1
rlin18 <rlin18@ur.rochester.edu>
2019-10-03 11:00:07 -0400

    A Rmarkdown for data visualization

---
+++ dogs/R/dog_function.R
@@ -0,0 +1,14 @@
+#' A Dog Fuction
+#'
+#' it is really fun
+
+
+
+
+dog_function <- function(love = TRUE){
+  if(love == TRUE){print("I love dogs")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
